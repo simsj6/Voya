84c5a9ab46b22a97d1f0e9a6a5d52876dcfd9712
simsj6 <simsj6@wwu.edu>
2026-05-11 13:00:20 -0700

    Import assets

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,37 +10,7 @@ import Profile from '../../pages/Profile/Profile';
 import SignInForm from '../../forms/SignInForm';
 import SignupForm from '../../forms/SignupForm';
 import AddTrip from '../../pages/AddTrip/AddTrip';
-
-// This will be fetched from some api later, this is for testing
-const assets = {
-    beach: "/assets/voya-beach.png",
-    alaska: "/assets/voya-alaska.jpg",
-    aurora: "/assets/voya-aurora.jpg",
-    lake: "/assets/voya-lake.jpg",
-    trees: "/assets/voya-trees.jpg",
-    alaskaView: "/assets/voya-alaska-view.png",
-    activity1: "/assets/voya-activity1.png",
-    activity2: "/assets/voya-activity2.png",
-    activity3: "/assets/voya-activity3.png",
-    activity4: "/assets/voya-activity4.png",
-    map: "/assets/voya-map.png",
-    villa: "/assets/voya-villa.jpg",
-    amalfi: "/assets/voya-amalfi.png",
-    kyoto: "/assets/voya-kyoto.png",
-    river: "/assets/voya-river.png",
-    tower: "/assets/voya-tower.png",
-    market: "/assets/voya-market.png",
-    coast: "/assets/voya-coast.png",
-    stonehenge: "/assets/voya-stonehenge.png",
-    oxford: "/assets/voya-oxford.png",
-    mountainView: "/assets/voya-mountain-view.jpg",
-    gallery1: "/assets/voya-gallery1.jpg",
-    gallery2: "/assets/voya-gallery2.jpg",
-    gallery3: "/assets/voya-gallery3.jpg",
-    gallery4: "/assets/voya-gallery4.jpg",
-    basemap: "/assets/voya-basemap.png",
-    profile: "/assets/voya-profile.jpg",
-};
+import { assets } from '../../constants/assets';
 
 const explorePopularCities = [
     ["Alaska: Westminster to Greenwich River Thames", assets.aurora, "2 Hours", "$35.00"],
@@ -58,7 +28,7 @@ export default function App() {
 
             <Routes>
                 <Route path="/" element={<HomePage explorePopularCities={explorePopularCities} />}/>
-                <Route path="/popular-destinations" element={<PopularDestinations />}/>
+                <Route path="/popular-destinations" element={<PopularDestinations cards={explorePopularCities} />}/>
                 <Route path="/plan-a-trip" element={<PlanTrip />}/>
                 <Route path="/map" element={<Map />}/>
                 <Route path="/profile" element={<Profile />}/>
