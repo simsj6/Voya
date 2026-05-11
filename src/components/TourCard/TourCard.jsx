7005d339a23a799ef78c74aebac74aef05369ce8
simsj6 <simsj6@wwu.edu>
2026-05-11 12:59:39 -0700

    Add TourCard component/styling

---
+++ src/components/TourCard/TourCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./TourCard.css";
+
+export default function TourCard({ title, image, duration, price }) {
+  return (
+    <article className="tour-card">
+      <img src={image} alt="" />
+      <div className="rating">4.8</div>
+      <h3>{title}</h3>
+      <div className="tags">
+        <span>{duration}</span>
+        <span>Walking</span>
+        <span>Family Plan</span>
+      </div>
+      <p>
+        <strong>{price}</strong> per person
+      </p>
+    </article>
+  );
+}
