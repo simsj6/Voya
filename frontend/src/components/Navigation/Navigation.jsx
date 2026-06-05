ba41a00418f57301ec4f578d753585bad293b6b0
simsj6 <simsj6@wwu.edu>
2026-06-05 12:08:40 -0700

    Hide sign in/sign up buttons when signed in

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -3,18 +3,23 @@ import { Link } from 'react-router-dom';
 import './Navigation.css';
 
 export default function Navigation({ active }) {
-    return (
-        <header className="nav">
-            <Link className="brand" to="/">VOYA</Link>
-            <nav className="nav-links" aria-label="Main navigation">
-                <Link className={active === "/" ? "active" : ""} to="/">Home</Link>
-                <Link className={active === "/popular-destinations" ? "active" : ""} to="/popular-destinations">Popular Destinations</Link>
-                <Link className={active === "/plan-a-trip" ? "active" : ""} to="/plan-a-trip">Plan A Trip</Link>
-                <Link className={active === "/add-trip" ? "active" : ""} to="/add-trip">Add Trip</Link>
-                <Link className={active === "/profile" ? "active" : ""} to="/profile">User Profile</Link>
-                <Link className="signin" to="/signin">Sign In</Link>
-                <Link className="signup" to="/signup">Sign Up</Link>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="nav">
+      <Link className="brand" to="/">VOYA</Link>
+      <nav className="nav-links" aria-label="Main navigation">
+        <Link className={active === "/" ? "active" : ""} to="/">Home</Link>
+        <Link className={active === "/popular-destinations" ? "active" : ""} to="/popular-destinations">Popular Destinations</Link>
+        <Link className={active === "/plan-a-trip" ? "active" : ""} to="/plan-a-trip">Plan A Trip</Link>
+        <Link className={active === "/add-trip" ? "active" : ""} to="/add-trip">Add Trip</Link>
+        {localStorage.getItem("User") ?
+          <Link className="signup" to="/profile">User Profile</Link>
+        :
+          <>
+            <Link className="signin" to="/signin">Sign In</Link>
+            <Link className="signup" to="/signup">Sign Up</Link>
+          </>
+        }
+      </nav>
+    </header>
+  );
 }
